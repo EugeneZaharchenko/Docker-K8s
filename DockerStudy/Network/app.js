4a9bb51abf6a2d54ea2ddb4311c7ec154da0f5b6
Y.Zakharchenko <Y.Zakharchenko@steel.mil.ua>
2025-02-11 14:00:30 +0200

    mongodb connection from cont to local

--- DockerStudy/Network/app.js
+++ DockerStudy/Network/app.js
@@ -62,17 +62,17 @@ app.get("/people", async (req, res) => {
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
-async function connectToDatabase() {
-  try {
-    await mongoose.connect("mongodb://host.docker.internal:27017/swfavorites", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-}
+// Local mongo connection
+// async function connectToDatabase() {
+//   try {
+//     await mongoose.connect("mongodb://host.docker.internal:27017/swfavorites", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//   }
+// }
 
 connectToDatabase();
